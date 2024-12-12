6c9be38575b3bb3ad6647d94c335bc1aabc4ac50
quandt <hosyquan795@gmail.com>
2024-12-12 08:49:33 +0700

    lần 2

--- SymphonyLimited/obj/Debug/net8.0/SymphonyLimited.AssemblyInfo.cs
+++ SymphonyLimited/obj/Debug/net8.0/SymphonyLimited.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SymphonyLimited")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b86975c6e685096593db07821d82c8b47fd919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fa3df202a4847b702a5be0e0a52b154f909786")]
 [assembly: System.Reflection.AssemblyProductAttribute("SymphonyLimited")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SymphonyLimited")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
